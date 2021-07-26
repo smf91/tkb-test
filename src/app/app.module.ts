72554489592bf82f15d7b74b16ad2e577bb23068
Alexey Samofalov <samofalov91@gmail.com>
2021-07-27 01:40:56 +0300

    added input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { InputComponentComponent } from './shared/components/input-component/input-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    InputComponentComponent
   ],
   imports: [
     BrowserModule,
